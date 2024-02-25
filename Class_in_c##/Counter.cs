64767a82fa2cb714cf6345fa9632f257890fd43a
ASUS <ASUS@DESKTOP-BDU1F2V>
2024-02-25 22:33:43 +0400

    Proje dosyası ekle.

---
+++ Class_in_c##/Counter.cs
@@ -0,0 +1,68 @@
+﻿namespace Class_in_c_
+{
+    public class Counter
+    {
+        private int min, max, current;
+
+        public Counter(int min, int max)
+        {
+            this.Min = min;
+            this.Max = max;
+            this.current = min;
+        }
+        public int Min
+        {
+            get { return min; }
+            set
+            {
+                if (value >= 0)
+                {
+                    min = value;
+                    return;
+
+                }
+                else
+                    Console.WriteLine("Minimum 0 daxil edile biler");
+            }
+        }
+        public int Max
+        {
+            get { return max; }
+            set
+            {
+                if (value <= 100)
+                {
+                    max = value;
+                    return;
+
+                }
+                else
+                    Console.WriteLine("Maksimum 100 daxil edile biler");
+            }
+        }
+        public void increment()
+        {
+            if (current == max)
+            {
+                Console.WriteLine("Max ededi kecmek olmaz");
+
+            }
+            else
+                current++;
+        }
+        public void decrement()
+        {
+            if (current == min)
+            {
+                Console.WriteLine("Min ededi kecmek olmaz");
+
+            }
+            else
+                current--;
+        }
+        public void showdata()
+        {
+            Console.WriteLine($"Min-->{min},Max-->{max},Current-->{current}");
+        }
+    }
+}
---
+++ Class_in_c##/Fraction.cs
@@ -0,0 +1,81 @@
+﻿
+namespace Class_in_c_
+{
+    internal class Fraction
+    {
+        private int suret, mexrec;
+        public Fraction()
+        {
+            this.suret = 0;
+            this.mexrec = 1;
+        }
+        public Fraction(int suret, int mexrec)
+        {
+            this.Suret = suret;
+            this.Mexrec = mexrec;
+        }
+        public int Suret
+        {
+            get { return suret; }
+            set
+            {
+                if (value >= 0)
+                {
+                    suret = value;
+
+                }
+                else
+                    Console.WriteLine("Suret minimum 0 ola biler");
+
+            }
+
+        }
+
+        public int Mexrec
+        {
+            get { return mexrec; }
+            set
+            {
+                if (value > 0)
+                {
+                    mexrec = value;
+
+                }
+                else
+                    Console.WriteLine("Mexrec  0 ola bilmez");
+
+            }
+
+        }
+
+        public Fraction cem(Fraction fraction)
+        {
+            int yeniSuret = this.suret * fraction.mexrec + this.mexrec * fraction.suret;
+            int yeniMexrec = this.mexrec * fraction.mexrec;
+            return new Fraction(yeniSuret, yeniMexrec);
+        }
+        public Fraction ferq(Fraction fraction)
+        {
+            int yeniSuret = this.suret * fraction.mexrec - this.mexrec * fraction.suret;
+            int yeniMexrec = this.mexrec * fraction.mexrec;
+            return new Fraction(yeniSuret, yeniMexrec);
+        }
+        public Fraction hasil(Fraction fraction)
+        {
+            int yeniSuret = this.suret * fraction.suret;
+            int yeniMexrec = this.mexrec * fraction.mexrec;
+            return new Fraction(yeniSuret, yeniMexrec);
+        }
+        public Fraction qaliq(Fraction fraction)
+        {
+            int yeniSuret = this.suret * fraction.mexrec;
+            int yeniMexrec = this.mexrec * fraction.suret;
+            return new Fraction(yeniSuret, yeniMexrec);
+        }
+        public override string ToString()
+        {
+            return suret + "/" + mexrec;
+        }
+    }
+}
+
